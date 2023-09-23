2dbe72e7cb69a0b1a89cab9a02cbb96602cc3220
Victor-Gabriel Podaru <victorpodaru@gmail.com>
2023-09-23 15:47:52 +0200

    first commit

---
+++ ClasaCalculator/Calculator.py
@@ -0,0 +1,31 @@
+class MiniCalculator:
+    def __init__(self, zero = 0.0):
+        self.zero = zero #valoarea de start a calculatorului
+    def operatii_mini_calcualtor(self):
+        print(self.zero)
+        comanda = input('> ')
+        while comanda != 'x': #cat timp comanda este diferita de x calculatorul va face operatii succesive
+
+            if '+' in comanda:
+                numar  = float(comanda.split('+')[1]) #eliminam semnul  de calcul cu split, apoi convertim string-ul in float
+                self.zero = self.zero + numar         #[0] index semn, [1] index numar
+            elif '-' in comanda:
+                numar = float(comanda.split('-')[1])
+                self.zero = self.zero - numar
+            elif '*' in comanda:
+                numar = float(comanda.split('*')[1])
+                self.zero = self.zero * numar
+            elif'/' in comanda:
+                numar = float(comanda.split('/')[1])
+                self.zero = self.zero / numar
+            elif '=' in comanda:
+                numar = float(comanda.split('=')[1])
+                self.zero =  numar
+            else:
+                print(f'Invalid operation {self.zero}')
+
+            print(self.zero) #ne afiseaza rezultatul
+            comanda = input('>')# ne pune iar sa folosim o operatie si un numar
+
+
+
---
+++ Main/main.py
@@ -0,0 +1,8 @@
+from ClasaCalculator.Calculator import MiniCalculator #importarea clasei
+
+if __name__ == '__main__':
+    #intializare obiect
+    operatie_1 = MiniCalculator()
+    #apelare metoda
+    operatie_1.operatii_mini_calcualtor()
+
