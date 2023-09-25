648bc025a1807d83551a0285fb60cd26084edc59
Victor-Gabriel Podaru <victorpodaru@gmail.com>
2023-09-25 09:05:27 +0200

    Actualizare pt cazul cu numere negative

--- ClasaCalculator/Calculator.py
+++ ClasaCalculator/Calculator.py
@@ -6,45 +6,42 @@ class MiniCalculator:
         comanda = input('> ') #dam de la tastatura operatii urmate de numere
         while comanda != 'x': #cat timp comanda este diferita de x calculatorul va face operatii succesive
             try:
-                numar_ales = float(comanda.split(comanda[0])[1]) #in numar_ales, variabila diferita de numar stocam covertirea in flaot a stringului, eliminand semnul(+,=,-..) de pe poz. [0] cu functia split
+                operator_matematic = comanda[0] # se va alege mereu operatorul , acesta fiind mereu pe pozitia zero
+                numar_ales = float(comanda[1:]) #in numar_ales, variabila diferita de numar stocam covertirea in flaot a stringului, ignorand semnul(+,=,-..) de pe poz. [0]
+                # print(f'Numarul ales este {numar_ales}') #daca decomentam vom vedea cum a vem operator si numar ales
+                # print(f'Operatorul este {operator_matematic}')
+                if operator_matematic in ('+', '-', '*', '/'): #verificam existenta operatorilor
 
-                if isinstance(numar_ales , float): #verificam daca numarul este float, daca da , mergem in if-uri de sub el
-
-                    if '+' in comanda: #daca avem + in comanda de la stringul +40 de ex se executa
-                        numar = float(comanda.split('+')[1])  # eliminam semnul  de calcul cu split, apoi convertim string-ul in float
-                        self.zero = self.zero + numar  # [0] index semn, [1] index numar
+                    if operator_matematic == '+': #daca avem + in comanda de la stringul +40 de ex se executa
+                        self.zero = self.zero + numar_ales
                         print(self.zero)  # ne afiseaza rezultatul
 
-                    elif '-' in comanda:
-                        numar = float(comanda.split('-')[1])
-                        self.zero = self.zero - numar
+                    elif operator_matematic == '-':
+                        self.zero = self.zero - numar_ales
                         print(self.zero)  # ne afiseaza rezultatul
 
-                    elif '*' in comanda:
-                        numar = float(comanda.split('*')[1])
-                        self.zero = self.zero * numar
+                    elif operator_matematic == '*':
+                        self.zero = self.zero * numar_ales
                         print(self.zero)  # ne afiseaza rezultatul
 
-                    elif '/' in comanda:
-                        numar = float(comanda.split('/')[1])
-                        if numar == 0:  # imparitrea la 0 nu se poate
+                    elif operator_matematic == '/':
+                        if numar_ales == 0:  # imparitrea la 0 nu se poate
                             try:  # facem incercare si exceptie
-                                rezultat = self.zero / numar
+                                rezultat = self.zero / numar_ales
                             except ZeroDivisionError:
                              print(f'Invalid operation {self.zero}')  # exceptam eroarea si in loc de eroare printam mesajul
 
                         else:
-                            self.zero = self.zero / numar
+                            self.zero = self.zero / numar_ales
                             print(self.zero)  # ne afiseaza rezultatul
 
-                    elif '=' in comanda:
-                        numar = float(comanda.split('=')[1])
-                        self.zero = numar
-                        print(self.zero)  # ne afiseaza rezultatul
+                elif operator_matematic == '=':
+                    self.zero = numar_ales
+                    print(self.zero)  # ne afiseaza rezultatul
 
 
-                    else: #daca avem alte input-uri cu operatii gresite afisam invalidarea ex (%6)
-                        print(f'Invalid operation {self.zero}')
+                else: #daca avem alte input-uri cu operatii gresite afisam invalidarea ex (%6)
+                    print(f'Invalid operation {self.zero}')
 
             except ValueError: #exceptam eroarea daca ceea ce am introdus nu e float (+123faasd) si printam invalidarea
                 print(f'Invalid operation {self.zero}')
